312c621fdab55b1b8d26e982038d346508430f39
jbmagination <26027089+jbmagination@users.noreply.github.com>
2022-09-04 02:35:55 -0400

    Added console logging

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ const whitelistedBots = ['Mr_Sus_', 'matdoesdev']
 const blockedUsers = []
 
 async function startBot () {
+  console.log('Starting bot')
+
   if (config.server.online) {
     bot = mineflayer.createBot({
       host: config.server.host,
@@ -46,6 +48,8 @@ async function startBot () {
 }
 
 async function bindEvents (bot) {
+  console.log('Binding events')
+
   // to allow the JavaScript code to run. This should never be undefined, but if it is it will error and then cleanly exit
   if (bot == (undefined || null)) {
     console.error(`bot is ${bot}. THIS SHOULD NEVER HAPPEN. exiting`)
@@ -60,6 +64,7 @@ async function bindEvents (bot) {
     setTimeout(() => {
       introMessageSent = true
     }, 1)
+    console.log('Startup message sent')
   })
 
   bot.on('playerJoined', (player) => {
@@ -67,33 +72,44 @@ async function bindEvents (bot) {
       bot.chat(
         `Uptime check: I'm active! Type ${config.prefix}help for a list of commands`
       )
+      console.log('Uptime check message sent')
     }
   })
 
   async function commands (username, message, whisper) {
+    let whisperMessage;
+    if (whisper == true) whisperMessage = "whisper"; else whisperMessage = "chat message"
     if (message.startsWith(`${config.prefix} `)) return
     else if (
       knownBots.includes(username) &&
       !whitelistedBots.includes(username)
     ) {
-    } else if (blockedUsers.includes(username)) return
+      console.log(`${username} attempted to run "${config.prefix}${message}" in a ${whisperMessage} but they're a bot`)  
+      return
+    }
+    else if (blockedUsers.includes(username)) {
+      console.log(`${username} attempted to run "${config.prefix}${message}" in a ${whisperMessage} but they're blocked`)  
+      return
+    }
     else {
       if (message.startsWith(`${config.prefix}`)) {
         message = message.substring(config.prefix.length)
-        console.log(message)
       }
-      console.log(message)
+
       if (message == 'help' || message.startsWith('help ')) {
+        console.log(`${username} ran "${config.prefix}${message}" in a ${whisperMessage}`)  
         bot.whisper(
           username,
           `${config.prefix}online, ${config.prefix}hashcheck, ${config.prefix}seed, ${config.prefix}source`
         )
       } else if (message == 'seed' || message.startsWith('seed ')) {
+        console.log(`${username} ran "${config.prefix}${message}" in a ${whisperMessage}`)  
         bot.whisper(
           username,
           "The seed of the server is LiveOverflow61374546 (or 64149200) - See the original video 'They Cracked My Server!' at https://youtu.be/gSxcDYCK_lY"
         )
       } else if (message == 'source' || message.startsWith('source ')) {
+        console.log(`${username} ran "${config.prefix}${message}" in a ${whisperMessage}`)  
         bot.whisper(
           username,
           'The source code for the bot can be found at https://github.com/jbmagination/UnLagMachine'
@@ -101,6 +117,8 @@ async function bindEvents (bot) {
       }
       // #region | +hashcheck
       else if (message.startsWith('hashcheck')) {
+        console.log(`${username} ran "${config.prefix}${message}" in a ${whisperMessage}`)  
+        
         // #region | Known Honeypot Hashes
         const honeypotHashes = [
           '946C329913A360435724E3C587931A1900B47EEC1EFDA7BD9139AF923405A293',
@@ -200,6 +218,8 @@ async function bindEvents (bot) {
 
       // #region | +online
       else if (message === 'online') {
+        console.log(`${username} ran "${config.prefix}${message}" in a ${whisperMessage}`)  
+        
         let botCount
         let playerCount
         let playerList
@@ -253,6 +273,7 @@ async function bindEvents (bot) {
       (message.includes('timeouts') || message.includes('timed out')) &&
       username !== bot.username
     ) {
+      console.log(`${username} triggered the automatic timeout response`)  
       bot.chat(
         `This is an automated response. ${username}, if you're getting frequent timeouts then you may be on a honeypot. Run ${config.prefix}hashcheck to figure out whether or not you are.`
       )
